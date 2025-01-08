5515181ce18fb4a168a1534373615d066465f331
Yogesh Kale <125132155+yogeshkale09@users.noreply.github.com>
2025-01-08 16:55:23 +0530

    commit

---
+++ ecomsite/shop/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Products(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.FloatField()
+    discount_price = models.FloatField()
+    category = models.CharField(max_length=200)
+    description = models.TextField()
+    stock = models.FloatField()
+    image = models.CharField(max_length=300)
---
+++ ecomsite/shop/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from .models import Products
+from django.core.paginator import Paginator
+
+# Create your views here.
+
+def index(request):
+    product_objects = Products.objects.all().order_by('id')
+    #search code
+    item_name=request.GET.get('item_name')
+    if item_name !='' and item_name is not None:
+        product_objects=product_objects.filter(title__icontains=item_name)
+    
+    #Paginator code
+    paginator=Paginator(product_objects,4)
+    page=request.GET.get('page')
+    product_objects = paginator.get_page(page)
+        
+    return render(request,'shop/index.html',{'product_objects':product_objects})
+
+
+#creating detail views of products
+def detail(request,id):
+    product_object=Products.objects.get(id=id)
+    return render(request,'shop/detail.html',{'product_object':product_object})
